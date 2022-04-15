41b7527d4d1e1a483ff4f31d7c95f3ced9f8ab4f
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2022-04-15 09:07:55 +0200

    Add package startup message

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onAttach <- function(...) {
+  packageStartupMessage("
+PLEASE NOTE:
+Paramlink has been superseded by the `ped suite` packages (https://magnusdv.github.io/pedsuite/).
+It is maintained for legacy purposes only, and should not be used in new projects.
+")}
